7835c43b6db2d608d9e0f8eed6e2b6f92c2cdab0
FrankSinatra1 <smarttv201404@gmail.com>
2017-10-11 11:54:00 +0300

    first

---
+++ js/main.js
@@ -0,0 +1,11 @@
+//Menu-filter
+$(".hover_menuJS").click(function(){
+    $(".filter_clickMenu").toggleClass('filter_clickMenuJS');
+});
+
+
+$(".left_fixedBlock--menu").click(function(){
+   $(".wrap_menu").toggleClass("wrap_menuJS");
+   $(".wrap_listMenuJS").toggleClass("listMenuJS_ul");
+});
+
