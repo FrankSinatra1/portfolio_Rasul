039e0a7f7f08d6ab55730b288e8df29c3a2b8c91
FrankSinatra1 <smarttv201404@gmail.com>
2017-10-11 18:59:19 +0300

    menu and jobs

--- js/main.js
+++ js/main.js
@@ -7,5 +7,14 @@ $(".hover_menuJS").click(function(){
 $(".left_fixedBlock--menu").click(function(){
    $(".wrap_menu").toggleClass("wrap_menuJS");
    $(".wrap_listMenuJS").toggleClass("listMenuJS_ul");
+   $(".wrap_adressMenu-one").toggleClass("adressesMenuJS");
+   $(".wrap_adressMenu-two").toggleClass("adressesMenuJS");
+   $(".wrapper_fullContent").toggleClass("wrap_fullContentJS");
 });
 
+
+$(".application_messageMenuJS").click(function(){
+    $(".wrapper_formMenuJS").toggleClass("anim_formMenuJS");
+    $(".wrapper_adressMenu").toggleClass("anim_adressMenuJS");
+    $(this).toggleClass("application_messageMenuJS_fontColor");
+});
